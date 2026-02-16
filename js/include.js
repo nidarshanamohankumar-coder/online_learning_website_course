66ebb50038781655872643cb5c2393a1ffc85906
nidarshanamohankumar-coder <nidarshanamohankumar@gmail.com>
2026-02-16 13:40:51 +0530

    Delete js/include.js

--- js/include.js
+++
@@ -1,8 +0,0 @@
-function loadHTML(id, file) {
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    })
-    .catch(error => console.error("Error loading file:", error));
-}
